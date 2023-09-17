ba51582887062a3c75c4a2bdd5654f7de2b8e26d
Jorge <Jorge@DESKTOP-MSOVDS0>
2023-09-17 17:02:48 -0500

    ejercicio 11 resuelto

--- taller punto 11/Program.cs
+++ taller punto 11/Program.cs
@@ -6,29 +6,61 @@ using System;
 
 public class Punto11
 {
-static void Main(string[] args)
+    static void Main(string[] args)
     {
-        int n,num, contador = 0; // declarar variables
+        //int n,num, contador = 0; // declarar variables
 
-        Console.Write("Ingresar la cantidad de números que desea evaluar: ");
-        n = Convert.ToInt32(Console.ReadLine());
+        //Console.Write("Ingresar la cantidad de números que desea evaluar: ");
+        //n = Convert.ToInt32(Console.ReadLine());
 
-        do
+        //do
+        //{
+        //    Console.Write("Ingrese el numero: ");
+        //    num = Convert.ToInt32(Console.ReadLine());
+
+        //    if (num >= 0)
+        //    {
+        //        contador++;
+        //    }
+        //    else
+        //    {
+        //        break;
+        //    }
+        //}
+        //while (contador < n);
+
+        //Console.Write(String.Format("La cantidad de número positivos ingresados es de {0}", contador));*/
+        ///*int num, contador = 0; // declarar variables
+        //  bool isPositivo = true;
+        //  do
+        //  {
+        //    Console.Write("Ingrese el numero: ");
+        //    num = Convert.ToInt32(Console.ReadLine());
+
+        //      if (num >= 0)
+        //         {
+        //            contador++;
+        //         }
+        //         else
+        //         {
+        //            isPositivo = false;
+        //         }
+        //  }
+        //   while (isPositivo);
+
+        //   Console.Write(String.Format("La cantidad de número positivos ingresados es de {0}", contador));
+
+        int num, contador = 0; // declarar variables
+
+        Console.Write("Ingrese el numero: ");
+        num = Convert.ToInt32(Console.ReadLine());
+
+        while (num >= 0)
         {
-            Console.Write("Ingrese el numero: ");
+            contador++;
+            Console.Write("Ingrese otro número: ");
             num = Convert.ToInt32(Console.ReadLine());
-
-            if (num >= 0)
-            {
-                contador++;
-            }
-            else
-            {
-                break;
-            }
         }
-        while (contador < n);
-
         Console.Write(String.Format("La cantidad de número positivos ingresados es de {0}", contador));
     }
 }
