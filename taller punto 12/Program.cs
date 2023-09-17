47c8d2fafe7accc7f09bb4d636304c07cac4fe17
Jorge <Jorge@DESKTOP-MSOVDS0>
2023-09-17 18:28:23 -0500

    solucion punto 12

--- taller punto 12/Program.cs
+++ taller punto 12/Program.cs
@@ -3,5 +3,55 @@
 
 public class Punto12
 {
+    static void Main(string[] args)
+    {
+        int num1, num2, min, max, numero;
+        const string MESSAGE = "Ingrese un número valido. Presione la tecla enter"; //declaro variables
+        bool retorno = false;
 
+        do
+        {
+            Console.Clear();
+            Console.Write("Ingrese el primer número: "); //pido números
+            if (!int.TryParse(Console.ReadLine(), out num1))
+            {
+                Console.WriteLine(MESSAGE);
+                retorno = true;
+                Console.ReadKey();
+            }
+            else
+            {
+                retorno = false;
+            }
+        }
+        while (retorno);
+
+        do
+        {
+            Console.Clear();
+            Console.Write("Ingrese el segundo número: ");
+            if (!int.TryParse(Console.ReadLine(), out num2))
+            {
+                Console.WriteLine(MESSAGE);
+                retorno = true;
+                Console.ReadKey();
+            }
+            else
+            {
+                retorno = false;
+            }
+        }
+        while (retorno);
+        
+
+        min = Math.Min(num1, num2);
+        max = Math.Max(num1, num2);
+
+        numero = min;
+        while (numero <= max)
+        {
+            Console.Write(String.Format("{0} ", numero));
+            numero++;
+        }
+    }
 }
